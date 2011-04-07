7677db2cfd9e6c56a1ef74cba124104295caee42
Raptus <dev@raptus.com>
2011-04-07 15:25:13 +0000

    plone 4 compatiblitiy

--- raptus/contenttemplates/__init__.py
+++ raptus/contenttemplates/__init__.py
@@ -1,4 +1,2 @@
 from zope.i18nmessageid import MessageFactory
 raptusContenttemplatesMessageFactory = MessageFactory('raptus.contenttemplates')
-from Products.PlacelessTranslationService.utility import PTSTranslationDomain
-raptuscontenttemplatesdomain = PTSTranslationDomain('raptus.contenttemplates')
